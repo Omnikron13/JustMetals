9829f6efe62a3a99e450e073278383f4398f9ee0
Joey Sabey <joey.sabey@gmx.com>
2020-06-10 03:57:43 +0100

    Sort import list

--- src/main/java/omnikron13/justmetals/common/OreBlock.java
+++ src/main/java/omnikron13/justmetals/common/OreBlock.java
@@ -1,7 +1,7 @@
 package omnikron13.justmetals.common;
 
-import java.util.List;
 import java.util.ArrayList;
+import java.util.List;
 import net.minecraft.block.Block;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
