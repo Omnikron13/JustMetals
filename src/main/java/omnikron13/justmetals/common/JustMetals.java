ef8cc90eb6d39f0f551d2a3c01a2ef6f90369816
Joey Sabey <joey.sabey@gmx.com>
2020-06-10 23:26:05 +0100

    JustMetals class defers to Metal

--- src/main/java/omnikron13/justmetals/common/JustMetals.java
+++ src/main/java/omnikron13/justmetals/common/JustMetals.java
@@ -6,21 +6,28 @@ import net.minecraftforge.fml.common.Mod.Instance;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Mod(modid="justmetals", useMetadata=true)
 @EventBusSubscriber
 public final class JustMetals {
     public static final String MODID = "justmetals";
+    
+    public static List<Metal> Metals = new ArrayList<Metal>();
     
     @Instance
     public static JustMetals instance;
     
     @Mod.EventHandler
     public void preInit(FMLPreInitializationEvent event) {
-        new OreBlock("lithium", 1);
-        new IngotItem("lithium");
+        Metals.add(new Metal("lithium", 1));
     }
     
     @Mod.EventHandler
     public void init(FMLInitializationEvent event) {
+        for(Metal m : Metals) {
+            m.registerSmeltingRecipes();
+        }
     }
 }
