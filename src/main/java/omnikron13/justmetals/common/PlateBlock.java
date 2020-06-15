5bab518ab040e0da46718a6551872bfbcfd346b5
Joey Sabey <joey.sabey@gmx.com>
2020-06-15 03:00:36 +0100

    Refactored PlateBlock.registerRenders() to work with subdirectoried model files

--- src/main/java/omnikron13/justmetals/common/PlateBlock.java
+++ src/main/java/omnikron13/justmetals/common/PlateBlock.java
@@ -32,7 +32,7 @@ public class PlateBlock extends Block {
     public static final String TOOL = "pickaxe";
 
     protected static List<PlateBlock> Blocks = new ArrayList<PlateBlock>();
-    protected static List<Item> ItemBlocks = new ArrayList<Item>();
+    protected static List<ItemBlock> ItemBlocks = new ArrayList<>();
 
     protected String name;
     public ItemBlock itemBlock;
@@ -74,9 +74,9 @@ public class PlateBlock extends Block {
 
     @SubscribeEvent
     public static void registerRenders(ModelRegistryEvent event) {
-        for (Item i : ItemBlocks) {
-            ModelResourceLocation mrl = new ModelResourceLocation(i.getRegistryName(), "inventory");
-            ModelLoader.setCustomModelResourceLocation(i, 0, mrl);
+        for(PlateBlock b : Blocks) {
+            ModelResourceLocation mrl = new ModelResourceLocation(JustMetals.MODID + ":" + b.name + "/" + TYPE, "inventory");
+            ModelLoader.setCustomModelResourceLocation(b.itemBlock, 0, mrl);
         }
     }
 
